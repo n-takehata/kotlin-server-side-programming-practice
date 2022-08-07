2cbb19220a684ade6702a522e9e8f6ee79df1fc8
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-07 18:33:49 +0900

    version down redis

--- part2/book-manager/build.gradle.kts
+++ part2/book-manager/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     implementation("mysql:mysql-connector-java:8.0.23") // 追加
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.session:spring-session-data-redis")
-    implementation("redis.clients:jedis")
+    implementation("redis.clients:jedis:3.5.1")
     implementation("org.springframework.boot:spring-boot-starter-aop")
     mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0") // 追加
 
