59880882fed8c93da8238e565555760a6863c8f6
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-07 18:50:31 +0900

    add options for cors settings

--- part2/book-manager/src/main/kotlin/com/book/manager/presentation/controller/AdminBookController.kt
+++ part2/book-manager/src/main/kotlin/com/book/manager/presentation/controller/AdminBookController.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("admin/book")
-@CrossOrigin
+@CrossOrigin(origins = ["http://localhost:8081"], allowCredentials = "true")
 class AdminBookController(
     private val adminBookService: AdminBookService
 ) {
